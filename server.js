e557cddbfeee6bbe94ce11b488d3425f97dd9a4d
Aquib <aquibmahmood106@gmail.com.com>
2023-02-22 23:11:34 +0530

    Connect the client and Server End

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ const server=http.createServer(app)
 
 const io=socketio(server)
 
+io.on('connection',(socket)=>{
+    console.log('Socket is connected on id :-',socket.id)
+
+    socket.on('boom', ()=>{
+     console.log('boom received from', socket.id)
+    })
+
+
+    setInterval(()=>{
+        socket.emit('whizz')
+     },2000)
+
+     
+})
+
+
+
 app.use('/',express.static(__dirname+'/public'))
 server.listen(8000,()=>{
     console.log('Started on http://localhost:8000')
